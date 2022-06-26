de374cbbea23871891de49e8b87e6e35037c1546
DAVID BISSOU <davidebissou@gmail.com>
2022-06-25 21:32:50 -0500

    before .less colors

--- src/components/PokemonResources.jsx
+++ src/components/PokemonResources.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import { ResourceCard } from "./ResourceCard";
-import { Stack, Placeholder, Loader } from "rsuite";
+import { Stack, Input, InputGroup, Loader } from "rsuite";
+import SearchIcon from '@rsuite/icons/Search';
 
 export default function PokemonResources({resources}){
+    const [filter, setFilter] = useState("");
 
-    let resourceList = resources.map((obj) => <ResourceCard data={obj}/>)
-    console.log(resources[0])
-    if(resources.length == 0){
+    const re = new RegExp(filter,"i");
+
+    let filtered = resources.filter((obj) => re.test(obj.name) ? obj : null)
+
+    let resourceList = filtered.map((obj, index) => <ResourceCard key={index} data={obj}/>)
+
+    if(resources.length === 0){
         return(
            <Stack className={"dim-spaced"} wrap spacing={15}>
             <Loader center size={"lg"} content="Loading" />
@@ -15,8 +22,16 @@ export default function PokemonResources({resources}){
     }
 
     return(
-    <Stack className={"dim-spaced"} wrap spacing={15}>
-        {resourceList}
-    </Stack>
+        <div>
+            <InputGroup inside style={{width: "30%", margin: "10px 0"}} >
+                <Input placeholder="Filter search..." onChange={(text) => setFilter(text)}/>
+            <InputGroup.Button>
+            <SearchIcon />
+                </InputGroup.Button>
+            </InputGroup>
+            <Stack className={"dim-spaced"} wrap spacing={15}>
+                {resourceList}
+            </Stack>
+        </div>
     )
 }
